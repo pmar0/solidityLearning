f6843eeaed9b6078575f2aa8e4b405ec67537579
pmar0 <pmarino1994@gmail.com>
2022-01-19 16:32:38 -0500

    fix[deploy.js]: fixed account used

--- Ethereum_and_Solidity_The_Complete_Developers_Guide/inbox/deploy.js
+++ Ethereum_and_Solidity_The_Complete_Developers_Guide/inbox/deploy.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const HdWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-const provider = new HdWalletProvider(
+const provider = new HDWalletProvider(
     process.env.WALLET,
     process.env.NETWORK
 );
@@ -13,11 +13,12 @@ const web3 = new Web3(provider);
 const deploy = async () =>{
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Deploying from acocunt:',accounts[0])
+    console.log('Deploying from acocunt:',accounts[1])
     const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: '1000000', from: accounts[1] });
 
     console.log('Contract deployed to: ', result.options.address);
+    provider.engine.stop();
 };
 deploy();
