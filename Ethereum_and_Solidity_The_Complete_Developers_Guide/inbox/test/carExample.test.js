a859e73b23504a0ec818e8c8b04c20ab26c4e4d5
pmar0 <pmarino1994@gmail.com>
2022-01-19 15:44:21 -0500

    progress from 1-19-22

---
+++ Ethereum_and_Solidity_The_Complete_Developers_Guide/inbox/test/carExample.test.js
@@ -0,0 +1,33 @@
+/*
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    let car;
+
+    beforeEach(() =>{
+        car = new Car();
+    });
+
+    it('Park test', () => {
+        assert.equal(car.park(),'stopped');
+    });
+
+    it('Drive test', () => {
+        assert.equal(car.drive(),'vroom');
+    });
+});
+*/
