a64bbbc41d5f82c26399ab22cf082d195349b5df
pmar0 <pmarino1994@gmail.com>
2022-04-28 16:43:38 -0400

    completed hardhat tutorial

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,8 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const INFURA_API_KEY = process.env.ROPSTEN_API;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = process.env.KEY;
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: INFURA_API_KEY,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
